acfe9685437737cd2dd509bdfaa775347149ba07
Fuuun1024 <worapopfong@gmail.com>
2024-12-20 17:30:38 +0700

    lab11_1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,34 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+
+   
+  
+   string grade(int x)
+   {
+       if(x==0) return "A";
+       else if(x==1) return "B+";
+       else if(x==2) return "B";
+       else if(x==3) return "C+";
+       else if(x==4) return "C";
+       else if(x==5) return "D+";
+       else if(x==6) return "D";
+       else if(x==7) return "F";
+       else return "W";
+   }
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    string ent;
+    for(int i =0;i<3;i++){
+        cin.get();
+    }
+    srand(time(0));
+    int x = rand()%9;
+    cout << "You will get " << grade(x) << " in this 261102.";
+}
